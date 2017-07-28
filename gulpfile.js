213c7c7fb05a25a95131cdb2b6071a04d7af6f44
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-07-28 14:02:04 +0400

    Update - Modification des tasks gulps

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,64 @@
 var gulp = require('gulp'),
-    rename = require('gulp-rename'), //Rename files
-    sass = require('gulp-sass'), //Convertion SCSS -> CSS
+    browserSync = require('browser-sync'), //Asynchronous browser loading
+    reload = browserSync.reload,
+    autoprefixer = require('gulp-autoprefixer'), //Autoprefixing
     minifycss = require('gulp-minify-css'), // Minification CSS
+    filter = require('gulp-filter'), //Enables you to work on a subset of the original files by filtering them using globbing
+    uglify = require('gulp-uglify'), //Minifies JS
+    imagemin = require('gulp-imagemin'), //Minifies images
+    newer = require('gulp-newer'), //For passing through only those source files that are newer than corresponding destination files.
+    rename = require('gulp-rename'), //Rename files
+    concat = require('gulp-concat'), //Concat 2 JS files
+    notify = require('gulp-notify'), //Error messages
+    cmq = require('gulp-combine-media-queries'), //Combine Media queries
+    runSequence = require('gulp-run-sequence'), //Run a series of dependent gulp tasks in order
+    sass = require('gulp-sass'), //Convertion SCSS -> CSS
+    plugins = require('gulp-load-plugins')({ camelsize: true}), //To automatically load in gulp plugins
+    ignore = require('gulp-ignore'), //Helps with ignoring files/folders
+    rimraf = require('gulp-rimraf'), //Helps with removing files/folders
     plumber = require('gulp-plumber'), //No stop when errors
-    uglify = require('gulp-uglify'); //Minification JS
+    cache = require('gulp-cache'), //A cache proxy task for Gulp
+    sourcemaps = require('gulp-sourcemaps'); //Show sourcemaps on dev
 
-//SCSS Task
-gulp.task('css', function() {
-  return gulp.src('./src/style/sass/*.scss')
+//Browser Sync
+gulp.task('browser-sync', function() {
+  var files = [
+    '**/*.php',
+    '**/*.{png,jpg,gif}'
+  ];
+
+  browserSync.init(files, {
+    proxy: url,
+    injectChanges: true
+  });
+});
+
+//Style Task
+gulp.task('styles', function() {
+  return gulp.src('./app/style/sass/*.scss')
     .pipe(plumber())
-    .pipe(sass())
-    .pipe(minifyCss())
-    .pipe(gulp.dest('./src/style/'));
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'compact',
+      precision: 10
+    }))
+    .pipe(sourcemaps.write({includeContent: false}))
+    .pipe(sourcemaps.init({loadMpas: true}))
+    .pipe(autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('./'))
+    .pipe(filter('**/*.css'))
+    .pipe(cmq())
+    .pipe(reload({ stream: true }))
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(minifycss({
+      maxLineLen: 80
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(reload({ stream: true }))
+    .pipe(notify({ message: 'Styles Task Complete', onLast: true}));
 });
 
 //JS Task
