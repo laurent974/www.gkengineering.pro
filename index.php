0ed5a84dccc1f8625262dc860d72ffe998bce61c
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-03 12:26:16 +0400

    Add - Initialisation de l'index

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <title>okok</title>
+  </head>
+  <body>
+    <h1>hello</h1>
+
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/src/js/vendors.min.js"></script>
+    <!-- A supprimer en prod -->
+    <script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+
+  </body>
+</html>
