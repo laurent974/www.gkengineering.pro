786cf530e98ec768f52d57c5c959c253ebf1f178
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-09 11:18:43 +0400

    Add - Functions pour wordpress

--- functions.php
+++ functions.php
@@ -1,1 +1,52 @@
 <?php
+  // Header clean
+  remove_action( 'wp_head', 'rsd_link' );
+  remove_action( 'wp_head', 'wlwmanifest_link' );
+  remove_action( 'wp_head', 'wp_generator' );
+  remove_action( 'wp_head', 'start_post_rel_link' );
+  remove_action( 'wp_head', 'index_rel_link' );
+  remove_action( 'wp_head', 'adjacent_posts_rel_link' );
+  remove_action( 'wp_head', 'wp_shortlink_wp_head' );
+  remove_action( 'wp_head','qtranxf_wp_head_meta_generator');
+  remove_action( 'wp_head', 'wp_resource_hints', 2 );
+
+  //Menu
+  function register_my_menu() {
+    register_nav_menu('header-menu',__( 'Header Menu' ));
+  }
+  add_action( 'init', 'register_my_menu' );
+
+  function disable_wp_emojicons() {
+
+    // all actions related to emojis
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+    // filter to remove TinyMCE emojis
+    add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+  }
+  add_action( 'init', 'disable_wp_emojicons' );
+
+  function disable_emojicons_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+      return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+      return array();
+    }
+  }
+
+  // Footer Clean
+  function my_deregister_scripts(){
+    wp_deregister_script( 'wp-embed' );
+  }
+  add_action( 'wp_footer', 'my_deregister_scripts' );
+
+  // Remove p content
+  remove_filter ('the_excerpt', 'wpautop');
+
+?>
