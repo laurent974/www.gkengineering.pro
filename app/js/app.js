c775e8ad7ef3dac7e068f8ca4b326b21af716208
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-09 11:22:45 +0400

    Initialisation - de barbajs

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+var app = function() {
+  //Please note, the DOM should be ready
+  Barba.Pjax.start();
+}
+
+$(document).ready(function() {
+  //Please note, the DOM should be ready
+  Barba.Pjax.start();
+});
