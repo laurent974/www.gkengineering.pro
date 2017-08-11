589569f699888c38cc6df14bd2635dce01c47209
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-11 11:27:23 +0400

    update - Bootstrap navbar

--- header.php
+++ header.php
@@ -9,7 +9,34 @@
   	  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   	<![endif]-->
   	<?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.min.css">
   </head>
   <body>
-    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php bloginfo('url')?>">
+                <?php bloginfo('name'); ?>
+            </a>
+    </div>
+
+        <?php
+            wp_nav_menu( array(
+                'menu'              => 'header-menu',
+                'depth'             => 2,
+                'container'         => false,
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker())
+            );
+        ?>
+    </div>
+</nav>
     <div id="barba-wrapper">
