64ea3a8c3e806fd5664adab84bd1f4fbad502b41
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-14 11:35:56 +0400

    Genration des js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -117,13 +117,12 @@ var animData = {
   autoplay: false,
   animationData: AnimationData,
   rendererSettings: {
-    scaleMode: 'noScale'
+    scaleMode: 'noScale',
+    preserveAspectRatio: 'xMidYMid slice'
   }
 };
 
 var animation = bodymovin.loadAnimation(animData);
-
-//bodymovin.setSpeed(2);
 bodymovin.setQuality('low');
 
 
@@ -135,14 +134,14 @@ var Transition = Barba.BaseTransition.extend({
   },
   AnimateOut: function() {
     var that = this;
-    //return $(this.oldContainer).animate({opacity: 0}).promise();
 
     //Bodyovin animation
     return new Promise(function(resolve) {
+      //Bodymovin Controls
       animation.goToAndStop(0);
       animation.setDirection(1);
-
       animation.play();
+
       animation.onComplete = function() {
         resolve(true);
       }
@@ -152,23 +151,16 @@ var Transition = Barba.BaseTransition.extend({
     var that = this;
     var $el = $(this.newContainer);
 
-    // $el.css({
-    //   visibility: 'visible',
-    //   opacity: 0
-    // });
-
-    // $el.animate({opacity: 1}, 400, function() {
-    //   that.done();
-    // });
-
-    //Bodyovin animation
+    //Animation Control
     animation.goToAndStop(animation.totalFrames, true);
     animation.setDirection(-1);
-
     animation.play();
+
+    //Show Content
+    $(that.oldContainer).hide();
+    $el.show();
+
     animation.onComplete = function() {
-      $(that.oldContainer).hide();
-      $el.show();
       that.done();
     }
   }
