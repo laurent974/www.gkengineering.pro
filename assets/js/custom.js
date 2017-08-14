a2c518b8056a3f7162e866603b42e91012042799
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-14 10:55:18 +0400

    generation de js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -110,6 +110,23 @@ var prevReoladingSamePage = {
   }
 }
 
+var animData = {
+  container: document.getElementById("bm"),
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  animationData: AnimationData,
+  rendererSettings: {
+    scaleMode: 'noScale'
+  }
+};
+
+var animation = bodymovin.loadAnimation(animData);
+
+//bodymovin.setSpeed(2);
+bodymovin.setQuality('low');
+
+
 var Transition = Barba.BaseTransition.extend({
   start: function() {
     Promise
@@ -117,21 +134,42 @@ var Transition = Barba.BaseTransition.extend({
       .then(this.AnimateIn.bind(this));
   },
   AnimateOut: function() {
-    return $(this.oldContainer).animate({opacity: 0}).promise();
+    var that = this;
+    //return $(this.oldContainer).animate({opacity: 0}).promise();
+
+    //Bodyovin animation
+    return new Promise(function(resolve) {
+      animation.goToAndStop(0);
+      animation.setDirection(1);
+
+      animation.play();
+      animation.onComplete = function() {
+        resolve(true);
+      }
+    })
   },
   AnimateIn :function() {
     var that = this;
     var $el = $(this.newContainer);
 
-    $(this.oldContainer).hide();
+    // $el.css({
+    //   visibility: 'visible',
+    //   opacity: 0
+    // });
 
-    $el.css({
-      visibility: 'visible',
-      opacity: 0
-    });
+    // $el.animate({opacity: 1}, 400, function() {
+    //   that.done();
+    // });
 
-    $el.animate({opacity: 1}, 400, function() {
+    //Bodyovin animation
+    animation.goToAndStop(animation.totalFrames, true);
+    animation.setDirection(-1);
+
+    animation.play();
+    animation.onComplete = function() {
+      $(that.oldContainer).hide();
+      $el.show();
       that.done();
-    });
+    }
   }
 });
