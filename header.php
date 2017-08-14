e1e693a57c9001c0419e931a367f1ec1b4e1eb3e
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-14 11:01:46 +0400

    Update - Ajout d'une var global js pour l'url & ajout div bodymovin

--- header.php
+++ header.php
@@ -10,8 +10,14 @@
   	<![endif]-->
   	<?php wp_head(); ?>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.min.css">
+
+    <script type="text/javascript">
+  var templateUrl = '<?php echo get_template_directory_uri(); ?>';
+  </script>
   </head>
   <body>
+    <div id="bm"></div>
+
     <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
