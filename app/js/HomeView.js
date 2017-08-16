e62b947f500afed69efd0920e0be9f2a95d3629c
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-16 05:17:34 +0400

    Update - Chargement et destruction des js a l'initialisation de la vue homepage

--- app/js/HomeView.js
+++ app/js/HomeView.js
@@ -2,14 +2,17 @@ var HomeView = Barba.BaseView.extend({
   namespace: 'homepage',
   onEnter: function() {
     console.log('Homepage is ready and attached to the DOM');
+    SliderIndex.init();
   },
   onEnterCompleted: function() {
     console.log("Homepage Transition has just finished");
+
   },
   onLeave: function() {
     console.log("Homepage Transition for leave");
   },
   onLeaveCompleted: function() {
     console.log("Homepage container has just been removed from the DOM");
+    SliderIndex.destroyEvents();
   }
 });
