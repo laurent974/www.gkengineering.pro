64357d5164a9a6e33f9b9e9247dc729315bc8ec5
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-16 11:30:00 +0400

    Del - example.php

--- example.php
+++
@@ -1,7 +0,0 @@
-<?php /* Template Name: Example template */ ?>
-
-<?php get_header(); ?>
-  <div class="barba-container" data-namespace="examplepage">
-    <h1>hello from example page</h1>
-  </div>
-<?php get_footer(); ?>
