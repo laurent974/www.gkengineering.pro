78b2a88777cf12085028322b64e58730d951a779
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-17 10:57:48 +0400

    Update - Refacto clientslider and destroy

--- app/js/ClientSlider.js
+++ app/js/ClientSlider.js
@@ -1,22 +1,33 @@
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
+var ClientSlider = {
+  params: {
+    logos: ".customer-logos"
+  },
+  handleSlick: function() {
+    $("body").find(this.params.logos).slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
     });
-});
+  },
+  destroy: function() {
+    $(".body").find(this.params.logos).slick('unslick');
+  },
+  init: function() {
+    this.handleSlick();
+  }
+}
