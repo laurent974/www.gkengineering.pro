e66a84947f92886907e77b3a8230850bb7fe9cda
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-18 11:58:41 +0400

    Update - Augmentation de l'intervale entre les sllids

--- app/js/SliderIndex.js
+++ app/js/SliderIndex.js
@@ -44,7 +44,7 @@ var SliderIndex = {
     this.handleSwipe();
     this.loopCarouselImg();
     $("body").find(".carousel").carousel({
-      interval: 6000,
+      interval: 10000,
       pause: "false"
     });
   }
