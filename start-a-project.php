2ac7d18e6dcf498efec232ea03ea077ac96aae61
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-19 08:56:46 +0400

    Add - Template php start a project

---
+++ start-a-project.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: Start a Project*/ ?>
+
+<?php get_header(); ?>
+  <div class="barba-container" data-namespace="startprojectpage"><!-- .barba-container -->
+    <h1>hello from Start a Project page</h1>
+<?php get_footer(); ?>
