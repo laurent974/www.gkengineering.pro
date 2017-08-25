c6c49a3ca3a5bd6097dfa6b803fb857bde1003f8
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-08-25 06:00:08 +0400

    Add - Intégration html/php de start a project

--- start-a-project.php
+++ start-a-project.php
@@ -1,6 +1,29 @@
 <?php /* Template Name: Start a Project*/ ?>
 
 <?php get_header(); ?>
-  <div class="barba-container" data-namespace="startprojectpage"><!-- .barba-container -->
-    <h1>hello from Start a Project page</h1>
+  <div class="barba-container Global-WithMargin" data-namespace="startprojectpage"><!-- .barba-container -->
+
+    <div class="breadcrumb" typeof="BreadcrumbList" vocab="https://schema.org/">
+    	<?php if(function_exists('bcn_display')) {
+    		bcn_display();
+    	} ?>
+    </div>
+
+    <section class="Box Box--noMargin StartAProjectBox container-fluid row"><!-- .StartAProjectBox -->
+      <div class="Box-TitleBackground-Gothic"><!-- .Box-TitleBackground-Gothic -->
+        <div class="container"><!-- .container -->
+          <h2 class="Title Font-Black Font-White Font-Upper Title-Space col-md-6"><?php _e("Start a Project", "Page: Start a Project") ?></h2>
+        </div><!-- /.container -->
+      </div><!-- /.Box-TitleBackground-Gothic -->
+
+      <div class="container"><!-- .container -->
+        <article class="Box--mediumMargin StartAProjectBox-Introduction"><!-- .StartAProjectBox-Introduction -->
+          <?php echo get_field( "introduction" ); ?>
+        </article><!-- /.StartAProjectBox-Introduction -->
+
+        <div class="Box--mediumMargin StartAProjectBox-Form"><!-- .StartAProjectBox-Form -->
+          <?php echo do_shortcode( '[contact-form-7 id="171" title="English - Start a Project"]' ); ?>
+        </div><!-- /.StartBoxAProject-Form -->
+      </div><!-- /.container -->
+    </section><!-- /.StartAProjectBox -->
 <?php get_footer(); ?>
