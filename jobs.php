5c1f2cec21982f78f7cb76a8727c71c20a3bbf1d
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-09-04 05:37:09 +0400

    Update - Refacto de la partie php / le menu jobs a été déplacé dans la page jobs dans le back office

--- jobs.php
+++ jobs.php
@@ -16,33 +16,30 @@
       </div><!-- /.Box-TitleBackground-Gothic -->
 
       <div class="container Jobs-Content"><!-- .container -->
-        <?php
-          // The Query
-          $query = new WP_Query(array('post_type' => 'jobs_offer'));
-          query_posts( $query );
-          $the_count = $query->found_posts;
-          $inc = 0;
-        ?>
 
-        <?php if ( $the_count >= 1 ) : ?>
+        <?php if( have_rows('job') ): ?>
           <h3 class="Title Title-PaleSky Font-Upper Font-Black Jobs-Title"><?php _e("Jobs open :", "Page: Jobs") ?></h3>
           <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="false"><!-- .panel-group -->
             <?php
-            // The Loop
-            while ( $query->have_posts() ) : $query->the_post();
-            $inc++;
+              $inc = 0;
+
+              while( have_rows('job') ): the_row();
+                // vars
+                $inc++;
+                $title = get_sub_field('job_title');
+                $job_description = get_sub_field('job_description');
             ?>
               <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="heading<?php echo $inc ?>">
                   <h4 class="panel-title">
                     <a class="panel-collapseBar" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $inc ?>" aria-expanded="false" aria-controls="collapse<?php echo $inc ?>">
-                      <?php the_title(); ?> <i class="material-icons">&#xE145;</i><i class="material-icons" style="display:none;">&#xE15B;</i>
+                      <?php echo $title; ?> <i class="material-icons">&#xE145;</i><i class="material-icons" style="display:none;">&#xE15B;</i>
                     </a>
                   </h4>
                 </div>
                 <div id="collapse<?php echo $inc ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?php echo $inc ?>">
                   <div class="panel-body">
-                    <?php echo get_field( "job_description" ); ?>
+                    <?php echo $job_description; ?>
 
                     <a href="#" class="Button Button-Rounded Button-BlueLight Button-Apply Font-Upper Font-LightBlue no-barba"><?php _e("Apply", "Page: Jobs"); ?></a>
                   </div>
@@ -54,7 +51,7 @@
             wp_reset_query();
             ?>
           <?php else: ?>
-            <h3 class="Title Title-PaleSky Font-Upper Font-Black Jobs-Title"><?php _e("Jobs close :", "Page: Jobs") ?></h3>
+            <h3 class="Title Title-PaleSky Font-Upper Font-Black Jobs-Title"><?php _e("Recruitment is close :", "Page: Jobs") ?></h3>
             <p class="Jobs-NoOffer Font-Light Font-LightBlue"><?php _e("
 Sorry recruitment is currently closed. You can make a spontaneous application, we will study it. Thank you.", "Page: Jobs") ?></p>
           <?php endif; ?>
