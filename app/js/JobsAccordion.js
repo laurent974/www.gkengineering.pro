e6f775a47fe5affd634c1eb8ec66aa79e6fe0149
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-09-06 06:23:00 +0400

    Add - Fonction scroll on click sur le bouton apply

--- app/js/JobsAccordion.js
+++ app/js/JobsAccordion.js
@@ -5,8 +5,24 @@ var JobsAccordion = {
     plusContent: "&#xE145;",
     removeContent: "&#xE15B;"
   },
+
+  handleScrollTo: function() {
+    $('body').find('.Button-Apply').on('click', function(event) {
+      var target = $("body").find("." + $(this).data('anchor'));
+
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top - 80
+          }, 800);
+      }
+    });
+  },
+
   init: function() {
     var that = this;
+
+    this.handleScrollTo();
 
     $("body").find(this.params.collapseDiv).on('shown.bs.collapse', function(){
       $(this).parent().find(that.params.materialIcons).toggle();
