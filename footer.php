452e5f8242a35ce7f968a8a6859c2d684777fa6e
Yen-Pon Laurent <lyenpon974@gmail.com>
2017-10-03 12:31:24 +0400

    Fix - div mal fermé & Update linkedin link

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
                 <i class="material-icons">&#xE0C8;</i>
                 <div class="Address-Adr">
                   <div class="street-address">8AE street 261, Saugkat</div>
-                  <div class="locality">Boeung salang</div >
-                  <div class="postal-code">Phnom Penh</div >
-                  <div class="country-name">Cambodia</div >
+                  <div class="locality">Boeung salang</div>
+                  <div class="postal-code">Phnom Penh</div>
+                  <div class="country-name">Cambodia</div>
                 </div>
                 <div class="tel"><i class="material-icons">&#xE0D4;</i> +855 (0) 23 987 770</div>
                 <div class="email"><i class="material-icons">&#xE0E1;</i> contact@gkengineering.pro</div>
@@ -26,7 +26,7 @@
               <h4 class="Title Font-White Font-Bold"><?php _e("Follow Us", "Page: All") ?></h4>
               <div class="Footer-SocialIcons"><!-- .Footer-SocialIcons -->
                 <a target="_blank" href="https://fb.me/Gkengineering.pro" class="FooterSocial-Icon FooterSocial-Icon--fb no-barba"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social-facebook.png" alt="Facebook"></a>
-                <a target='_blank' href="https://linkedin.com/" class="FooterSocial-Icon FooterSocial-Icon--in no-barba"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social-linkedin.png" alt="linkedin"></a>
+                <a target='_blank' href="https://www.linkedin.com/company/18241688/" class="FooterSocial-Icon FooterSocial-Icon--in no-barba"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/social-linkedin.png" alt="linkedin"></a>
               </div><!-- /.Footer-SocialIcons -->
             </section><!-- /.Footer-Box -->
           </div>
